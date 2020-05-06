ca302fc2bb080de1ec781ab963b801f0938d36c4
Reffx <ripsli@hotmail.com>
2020-05-06 18:20:33 +0200

    query my watches added

--- web-app/network/network.js
+++ web-app/network/network.js
@@ -649,6 +649,42 @@ module.exports = {
     },
 
     // query all cars transaction
+    queryMyWatches: async function (userId) {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+    
+    try {
+        // Create a new gateway for connecting to our peer node.
+        const gateway2 = new Gateway();
+        await gateway2.connect(ccp, { wallet, identity: userId, discovery: gatewayDiscovery });
+    
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway2.getNetwork('mychannel');
+    
+        // Get the contract from the network.
+        const contract = network.getContract('anticounterfeiting');
+
+        console.log(`\nGet watches transactions state for ${userId}`);
+        let result = await contract.evaluateTransaction('QueryMyWatches', userId);
+        result = JSON.parse(result.toString());
+        console.log(result);
+
+        // Disconnect from the gateway.
+        await gateway2.disconnect();
+
+        return result;
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        response.error = error.message;
+        return response;
+        }
+    },
+
+    // query all cars transaction
     queryAllWatches: async function (cardId, userType, userId) {
 
             // Create a new file system based wallet for managing identities.
