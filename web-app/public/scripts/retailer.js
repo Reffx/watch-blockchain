86fcf64eeeb93f7d1940965ab431e2682dd9f3c1
Reffx <ripsli@hotmail.com>
2020-05-20 00:00:31 +0200

    retailer.js created

---
+++ web-app/public/scripts/retailer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let apiUrl = location.protocol + '//' + location.host + '/api/';
+
+//check user input and call server
+function updateRetailer() {
+    //todo
+}
+    
+
+//check user input and call server
+$('.sign-in-retailer').click(function() {
+    updateRetailer();
+});
+
+//check user input and call server
+$('.sell-watch').click(function() {
+ //todo
+});
