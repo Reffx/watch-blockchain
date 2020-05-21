1080c933748bd7d6567e6b5b84c215c2f81cc0e8
Reffx <ripsli@hotmail.com>
2020-05-21 16:45:36 +0200

    retailer.js created updateRetailer.js

--- web-app/public/scripts/retailer.js
+++ web-app/public/scripts/retailer.js
@@ -4,8 +4,94 @@ let apiUrl = location.protocol + '//' + location.host + '/api/';
 
 //check user input and call server
 function updateRetailer() {
-    //todo
-}
+    //get user input data
+    let formRetailerName = $('.retailerName input').val();
+    let formPassword = $('.password input').val();
+
+    //create json data
+    let inputData = '{' + '"retailerName" : "' + formRetailerName + '", ' + '"password" : "' + formPassword + '"}';
+    console.log(inputData);
+
+    //make ajax call
+    $.ajax({
+        type: 'POST',
+        url: apiUrl + 'retailerData',
+        data: inputData,
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function () {
+            //display loading
+            document.getElementById('loader').style.display = 'block';
+        },
+        success: function (data) {
+
+            //remove loader
+            document.getElementById('loader').style.display = 'none';
+
+            //check data for error
+            if (data.error) {
+                alert(data.error);
+                return;
+            } else {
+
+                //update heading
+                $('.heading').html(function () {
+                    let str = '<h2><b> ' + data.name + ' </b></h2>';
+                    str = str + '<h2><b> ' + data.email + ' </b></h2>';
+
+                    return str;
+                });
+
+                //update dashboard
+                $('.dashboards').html(function () {
+                    let str = '';
+                    str = str + '<h5>Total points allocated to customers: ' + data.pointsGiven + ' </h5>';
+                    str = str + '<h5>Total points redeemed by customers: ' + data.pointsCollected + ' </h5>';
+                    return str;
+                });
+
+                //update use points transaction
+                $('.query-myWatches-transactions').html(function () {
+                    let str = '';
+                    let transactionData = data.queryMyWatchesResults;
+                    console.log(data.queryMyWatchesResults);
+
+                    for (let i = 0; i < transactionData.length; i++) {
+                        str = str + '<p>timeStamp: ' + transactionData[i].timestamp + '<br />owner: ' + transactionData[i].owner + '<br />manufacturer: ' + transactionData[i].manufacturer + '<br />WatchId: ' + transactionData[i].watchId + '<br />model: ' + transactionData[i].model + '<br />color: ' + transactionData[i].color + '<br />transactionID: ' + transactionData[i].transactionId + '</p><br>';
+                    }
+                    return str;
+                });
+
+                //update use points transaction
+                $('.query-watches-transactions').html(function () {
+                    let str = '';
+                    let transactionData = data.queryAllWatchesResults;
+                    console.log(data.queryAllWatchesResults);
+
+                    for (let i = 0; i < transactionData.length; i++) {
+                        str = str + '<p>timeStamp: ' + transactionData[i].timestamp + '<br />owner: ' + transactionData[i].owner + '<br />manufacturer: ' + transactionData[i].manufacturer + '<br />WatchId: ' + transactionData[i].watchId + '<br />model: ' + transactionData[i].model + '<br />color: ' + transactionData[i].color + '<br />transactionID: ' + transactionData[i].transactionId + '</p><br>';
+                    }
+                    return str;
+                });
+
+                //remove login section
+                document.getElementById('loginSection').style.display = 'none';
+                //display transaction section
+                document.getElementById('transactionSection').style.display = 'block';
+            }
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            //reload on error
+            alert('Error: Try again');
+            console.log(errorThrown);
+            console.log(textStatus);
+            console.log(jqXHR);
+
+            location.reload();
+        }
+    });
+};
     
 
 //check user input and call server
