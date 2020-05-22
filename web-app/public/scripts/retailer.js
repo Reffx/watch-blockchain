3aca6842363f9ac57d3996e8d62e354161055666
Reffx <ripsli@hotmail.com>
2020-05-23 01:43:19 +0200

    sellWatches added to retailer

--- web-app/public/scripts/retailer.js
+++ web-app/public/scripts/retailer.js
@@ -62,6 +62,26 @@ function updateRetailer() {
                     return str;
                 });
 
+                //update manufacturers dropdown for earn points transaction
+                $('.maintenance-manufacturer-id select').html(function() {
+                    let str = '<option value="" disabled="" selected="">select</option>';
+                    let transactionData = data.queryMyWatchesResults;
+                    for (let i = 0; i < transactionData.length; i++) {
+                        str = str + '<option manufacturer-id=' + transactionData[i].manufacturer + '> ' + transactionData[i].manufacturer + '</option>';
+                    }
+                    return str;
+                });
+
+                //update manufacturers dropdown for earn points transaction
+                $('.sell-myWatch-id select').html(function() {
+                    let str = '<option value="" disabled="" selected="">select</option>';
+                    let transactionData = data.queryMyWatchesResults;
+                    for (let i = 0; i < transactionData.length; i++) {
+                        str = str + '<option sell-my-watch-id=' +transactionData[i].manufacturer + "*+$+*" + transactionData[i].watchId + '> ' + transactionData[i].manufacturer + ": " + transactionData[i].watchId + '</option>';
+                    }
+                    return str;
+                });
+
                 //update use points transaction
                 $('.query-watches-transactions').html(function () {
                     let str = '';
@@ -99,8 +119,12 @@ $('.add-maintenance').click(function () {
     //get user input data
     let formRetailerName = $('.retailerName input').val();
     let formWatchId = $('.maintenance-watchid-id input').val();
-    let formManufacturerName = $('.maintenance-manufacturer-id input').val();
     let formInfo = $('.maintenance-info-id input').val();
+    let formManufacturerName = $('.maintenance-manufacturer-id select').find(':selected').attr('manufacturer-id');
+    if (!formManufacturerName) {
+        alert('Select manufacturer first');
+        return;
+    }
 
     //create json data
     let inputData = '{' + '"retailerName" : "' + formRetailerName + '", ' + '"watchId" : "' + formWatchId + '", ' + '"manufacturerName" : "' + formManufacturerName + '", ' + '"maintenanceInfo" : "' + formInfo + '"}';
@@ -150,5 +174,57 @@ $('.sign-in-retailer').click(function() {
 
 //check user input and call server
 $('.sell-watch').click(function() {
- //todo
+    
+    //select logic
+    let formManufacturerAndWatchId = $('.sell-myWatch-id select').find(':selected').attr('sell-my-watch-id');
+    if (!formManufacturerAndWatchId) {
+        alert('Select watch first');
+        return;
+    }
+    let res = formManufacturerAndWatchId.split("*+$+*");
+
+    let formRetailerName = $('.retailerName input').val();
+    let formManufacturerName = res[0];
+    let formWatchId = res[1];
+    let formOwner = $('.newOwner-id input').val();
+
+
+    //create json data
+    let inputData = '{' + '"watchId" : "' + formWatchId + '", ' + '"manufacturerName" : "' + formManufacturerName + '", ' + '"oldOwner" : "' + formRetailerName + '", ' + '"newOwner" : "' + formOwner + '"}';
+    console.log(inputData);
+
+    //make ajax call
+    $.ajax({
+        type: 'POST',
+        url: apiUrl + 'changeWatchOwner',
+        data: inputData,
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function () {
+            //display loading
+            document.getElementById('loader').style.display = 'block';
+        },
+        success: function (data) {
+
+            document.getElementById('loader').style.display = 'none';
+            //check data for error
+            if (data.error) {
+                alert(data.error);
+                return;
+            } else {
+                //update member page and notify successful transaction
+                // createWatch();
+                alert('Transaction successful');
+                updateRetailer();
+            }
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Error: Try again');
+            console.log(errorThrown);
+            console.log(textStatus);
+            console.log(jqXHR);
+        }
+    });
 });
