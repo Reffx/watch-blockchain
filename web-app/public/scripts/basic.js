7294b770e8ae3760c7d54a37bf644e9c76ed93fc
Reffx <ripsli@hotmail.com>
2020-05-25 12:59:45 +0200

    searchManufacturer added to member

--- web-app/public/scripts/basic.js
+++ web-app/public/scripts/basic.js
@@ -69,9 +69,7 @@ $('.search-watch').click(function(formUserId) {
 });
 
 //check user input and call server
-$('.search-retailer').click(searchRetailer);
-
-function searchRetailer() {
+$('.search-retailer').click(function searchRetailer() {
     let formUserId;
     if ($('.memberName input').val() != ""){
         formUserId = $('.memberName input').val();
@@ -114,7 +112,7 @@ function searchRetailer() {
                         str = '<h3>No retailer with this name found!</h3>';
                     } else {
                         str = '<div style="background:#e9ecef; width:100%;  border-radius:0.3rem; padding:20px;">'
-                        str = str + '<h3>Information about ' + transactionData.id + '</h3>';
+                        str = str + '<h3>Information about ' + transactionData.name + '</h3>';
                         str = str + '<p>'+ transactionData.name +'</p>';
                         str = str + '<p> '+ transactionData.address +'</p>';
                         str = str + '<p> '+ transactionData.zipCode + " " + transactionData.place +'</p>';
@@ -136,4 +134,73 @@ function searchRetailer() {
             console.log(jqXHR);
         }
     });
-}
+});
+
+//check user input and call server
+$('.search-manufacturer').click(function searchManufacturer() {
+    
+    let formUserId;
+    if ($('.memberName input').val() != ""){
+        formUserId = $('.memberName input').val();
+    }
+
+    //select logic
+    let formManufacturerName = $('.searchManufacturerName-id input').val();
+
+
+    //create json data
+    let inputData = '{' + '"userId" : "' + formUserId + '", ' + '"manufacturerName" : "' + formManufacturerName + '"}';
+    console.log(inputData);
+
+    //make ajax call
+    $.ajax({
+        type: 'POST',
+        url: apiUrl2 + 'searchManufacturer',
+        data: inputData,
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function () {
+            //display loading
+            document.getElementById('loader').style.display = 'block';
+        },
+        success: function (data) {
+
+            document.getElementById('loader').style.display = 'none';
+            //check data for error
+            if (data.error) {
+                alert(data.error);
+                return;
+            } else {
+                //update member page and notify successful transaction
+                //update use points transaction
+                $('.get-searchManufacturer-transactions').html(function () {
+                    let str = '';
+                    let transactionData = data;
+                    console.log(data.getMyWatchesResults);
+                    if (transactionData.length === 0) {
+                        str = '<h3>No manufacturer with this name found!</h3>';
+                    } else {
+                        str = '<div style="background:#e9ecef; width:100%;  border-radius:0.3rem; padding:20px;">'
+                        str = str + '<h3>Information about ' + transactionData.name + '</h3>';
+                        str = str + '<p>'+ transactionData.name +'</p>';
+                        str = str + '<p> '+ transactionData.address +'</p>';
+                        str = str + '<p> '+ transactionData.zipCode + " " + transactionData.place +'</p>';
+                        str = str + '<br>';
+                        str = str + '<p> '+ transactionData.email +'</p>';
+                        str = str + '<p> '+ transactionData.phoneNumber +'</p>';
+                    }
+                    str = str + '</div>'
+                    return str;
+                });
+            }
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Error: Try again');
+            console.log(errorThrown);
+            console.log(textStatus);
+            console.log(jqXHR);
+        }
+    });
+});
