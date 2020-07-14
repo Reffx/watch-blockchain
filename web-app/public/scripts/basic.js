2f30e095a8801803385abb0da9a31c9202bf71bc
Reffx <ripsli@hotmail.com>
2020-07-14 21:36:41 +0200

    search retailer manufacturer update

--- web-app/public/scripts/basic.js
+++ web-app/public/scripts/basic.js
@@ -63,11 +63,11 @@ $('.search-watch').click(function () {
                             str = str + 'owner: ' + transactionData[i].owner + '<br />';
                             str = str + 'manufacturer: ' + transactionData[i].manufacturer + '<br />';
                             str = str + 'watch-ID: ' + transactionData[i].watchId + '<br />';
-                            str = str + 'attribut1: ' + transactionData[i].attribut1 + '<br />';
-                            str = str + 'attribut2: ' + transactionData[i].attribut2 + '<br />';
-                            str = str + 'attribut3: ' + transactionData[i].attribut3 + '<br />';
-                            str = str + 'attribut4: ' + transactionData[i].attribut4 + '<br />';
-                            str = str + 'attribut5: ' + transactionData[i].attribut5 + '<br />';
+                            if (transactionData[i].attribut1 != '') { str = str + 'attribut1: ' + transactionData[i].attribut1 + '<br />'; }
+                            if (transactionData[i].attribut2 != '') { str = str + 'attribut2: ' + transactionData[i].attribut2 + '<br />'; }
+                            if (transactionData[i].attribut3 != '') { str = str + 'attribut3: ' + transactionData[i].attribut3 + '<br />'; }
+                            if (transactionData[i].attribut4 != '') { str = str + 'attribut4: ' + transactionData[i].attribut4 + '<br />'; }
+                            if (transactionData[i].attribut5 != '') { str = str + 'attribut5: ' + transactionData[i].attribut5 + '<br />'; }
                             str = str + 'transactionType: ' + transactionData[i].transactionType + '<br />';
                             str = str + 'transaction-executor: ' + transactionData[i].transaction_executor + '<br />';
                             str = str + 'information-verification: ' + transactionData[i].verified_information + '<br />';
@@ -116,62 +116,66 @@ $('.search-retailer').click(function searchRetailer() {
     //select logic
     let formRetailerName = $('.searchRetailerName-id input').val();
 
+    if (formRetailerName.length < 1) {
+        alert("Enter a retailer!");
+    } else {
 
-    //create json data
-    let inputData = '{' + '"userId" : "' + formUserId + '", ' + '"retailerName" : "' + formRetailerName + '"}';
-    console.log(inputData);
+        //create json data
+        let inputData = '{' + '"userId" : "' + formUserId + '", ' + '"retailerName" : "' + formRetailerName + '"}';
+        console.log(inputData);
 
-    //make ajax call
-    $.ajax({
-        type: 'POST',
-        url: apiUrl2 + 'searchRetailer',
-        data: inputData,
-        dataType: 'json',
-        contentType: 'application/json',
-        beforeSend: function () {
-            //display loading
-            document.getElementById('loader').style.display = 'block';
-        },
-        success: function (data) {
+        //make ajax call
+        $.ajax({
+            type: 'POST',
+            url: apiUrl2 + 'searchRetailer',
+            data: inputData,
+            dataType: 'json',
+            contentType: 'application/json',
+            beforeSend: function () {
+                //display loading
+                document.getElementById('loader').style.display = 'block';
+            },
+            success: function (data) {
 
-            document.getElementById('loader').style.display = 'none';
-            //check data for error
-            if (data.error) {
-                alert(data.error);
-                return;
-            } else {
-                //update member page and notify successful transaction
-                //update use points transaction
-                $('.get-searchRetailer-transactions').html(function () {
-                    let str = '';
-                    let transactionData = data;
-                    console.log(data.getMyWatchesResults);
-                    if (transactionData.length === 0 || transactionData.userType != "retailer") {
-                        str = '<h3>No retailer with this name found!</h3>';
-                    } else {
-                        str = '<div style="background:#e9ecef; width:100%;  border-radius:0.3rem; padding:20px;">'
-                        str = str + '<h3>Information about ' + transactionData.name + '</h3>';
-                        str = str + '<p>' + transactionData.name + '</p>';
-                        str = str + '<p> ' + transactionData.address + '</p>';
-                        str = str + '<p> ' + transactionData.zipCode + " " + transactionData.place + '</p>';
-                        str = str + '<br>';
-                        str = str + '<p> ' + transactionData.email + '</p>';
-                        str = str + '<p> ' + transactionData.phoneNumber + '</p>';
-                    }
-                    str = str + '</div>'
-                    return str;
-                });
+                document.getElementById('loader').style.display = 'none';
+                //check data for error
+                if (data.error) {
+                    alert(data.error);
+                    return;
+                } else {
+                    //update member page and notify successful transaction
+                    //update use points transaction
+                    $('.get-searchRetailer-transactions').html(function () {
+                        let str = '';
+                        let transactionData = data;
+                        console.log(data.getMyWatchesResults);
+                        if (transactionData.length === 0 || transactionData.userType != "retailer") {
+                            str = '<h3>No retailer with this name found!</h3>';
+                        } else {
+                            str = '<div style="background:#e9ecef; width:100%;  border-radius:0.3rem; padding:20px;">'
+                            str = str + '<h3>Information about ' + transactionData.name + '</h3>';
+                            str = str + '<p>' + transactionData.name + '</p>';
+                            str = str + '<p> ' + transactionData.address + '</p>';
+                            str = str + '<p> ' + transactionData.zipCode + " " + transactionData.place + '</p>';
+                            str = str + '<br>';
+                            str = str + '<p> ' + transactionData.email + '</p>';
+                            str = str + '<p> ' + transactionData.phoneNumber + '</p>';
+                        }
+                        str = str + '</div>'
+                        return str;
+                    });
+                }
+
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('Error: Try again');
+                console.log(errorThrown);
+                console.log(textStatus);
+                console.log(jqXHR);
             }
-
-
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert('Error: Try again');
-            console.log(errorThrown);
-            console.log(textStatus);
-            console.log(jqXHR);
-        }
-    });
+        });
+    }
 });
 
 //check user input and call server
@@ -191,6 +195,9 @@ $('.search-manufacturer').click(function searchManufacturer() {
     //select logic
     let formManufacturerName = $('.searchManufacturerName-id input').val();
 
+    if (formManufacturerName.length < 1) {
+        alert("Enter a manufacturer!");
+    } else {
 
     //create json data
     let inputData = '{' + '"userId" : "' + formUserId + '", ' + '"manufacturerName" : "' + formManufacturerName + '"}';
@@ -247,6 +254,7 @@ $('.search-manufacturer').click(function searchManufacturer() {
             console.log(jqXHR);
         }
     });
+    }
 });
 
 //used functions by member.js
@@ -354,11 +362,11 @@ function specificWatchTransactions(y) {
                             str = str + 'owner: ' + transactionData[i].owner + '<br />';
                             str = str + 'manufacturer: ' + transactionData[i].manufacturer + '<br />';
                             str = str + 'watch-ID: ' + transactionData[i].watchId + '<br />';
-                            str = str + 'attribut1: ' + transactionData[i].attribut1 + '<br />';
-                            str = str + 'attribut2: ' + transactionData[i].attribut2 + '<br />';
-                            str = str + 'attribut3: ' + transactionData[i].attribut3 + '<br />';
-                            str = str + 'attribut4: ' + transactionData[i].attribut4 + '<br />';
-                            str = str + 'attribut5: ' + transactionData[i].attribut5 + '<br />';
+                            if (transactionData[i].attribut1 != '') { str = str + 'attribut1: ' + transactionData[i].attribut1 + '<br />'; }
+                            if (transactionData[i].attribut2 != '') { str = str + 'attribut2: ' + transactionData[i].attribut2 + '<br />'; }
+                            if (transactionData[i].attribut3 != '') { str = str + 'attribut3: ' + transactionData[i].attribut3 + '<br />'; }
+                            if (transactionData[i].attribut4 != '') { str = str + 'attribut4: ' + transactionData[i].attribut4 + '<br />'; }
+                            if (transactionData[i].attribut5 != '') { str = str + 'attribut5: ' + transactionData[i].attribut5 + '<br />'; }
                             str = str + 'transaction-type: ' + transactionData[i].transactionType + '<br />';
                             str = str + 'transaction-executor: ' + transactionData[i].transaction_executor + '<br />';
                             str = str + 'information-verification: ' + transactionData[i].verified_information + '<br />';
